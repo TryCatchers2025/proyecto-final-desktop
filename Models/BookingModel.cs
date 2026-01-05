492352edea5cb6d96dddc50a246e29c8dbb7e1aa
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-01-05 01:03:26 +0100

    feat(models): add BookingModel properties

--- Models/BookingModel.cs
+++ Models/BookingModel.cs
@@ -2,5 +2,21 @@
 
 public class BookingModel
 {
+    public string? BookingId { get; set; }
+    public string? UserId { get; set; } = null!;
+    public string? RoomId { get; set; } = null!;
 
+    public string? StartDate { get; set; }
+    public string? EndDate { get; set; }
+    public string? BookingDate { get; set; }
+
+    public int Occupants { get; set; }
+
+    public decimal PricePerNight { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    public int? Discount { get; set; }
+    public int TotalNights { get; set; }
+
+    public string Status { get; set; } = null!; // 'active' | 'canceled'
 }
